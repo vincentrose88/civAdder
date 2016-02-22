60fdde0def4a7155e7db16cb3b68806ddc64bc2d
Laspimon <casperlehmann@gmail.com>
2016-02-22 17:20:32 +0100

    Abbreviate to start_word_num.

--- civ_battleroyal_leader_civ_adder.py
+++ civ_battleroyal_leader_civ_adder.py
@@ -53,10 +53,10 @@ updated_file = open(input_file + '_with_civs', 'w')
 for line in input_lines:
     new_line = []
     split_line = line.split(' ')
-    start_word_number = 0
-    word_number = 0
-    while word_number < len(split_line):
-        word=split_line[word_number]
+    start_word_num = 0
+    word_num = 0
+    while word_num < len(split_line):
+        word=split_line[word_num]
         if(word[-1] in '.,:;?!+-='):
             punct = word[-1]
             word = word[:-1]
@@ -68,21 +68,21 @@ for line in input_lines:
             while(word in all_names):
                 leader.append(word)
                 w += 1
-                word = split_line[word_number + w]
+                word = split_line[word_num + w]
 
             civ = find_best_leader_match(leader)
 
             if civ is not False:
                 new_line.extend(
-                    (' '.join(split_line[start_word_number:word_number]),
+                    (' '.join(split_line[start_word_num:word_num]),
                      ' {} ({}){} '.format(' '.join(leader), civ, punct)))
-                start_word_number = word_number + len(leader)
-                word_number = word_number + len(leader)
+                start_word_num = word_num + len(leader)
+                word_num = word_num + len(leader)
             else:
-                word_number += 1
+                word_num += 1
         else:
-            word_number += 1
+            word_num += 1
 
-    new_line.append(' '.join(split_line[start_word_number:]))
+    new_line.append(' '.join(split_line[start_word_num:]))
     updated_file.write(''.join(new_line))
 
